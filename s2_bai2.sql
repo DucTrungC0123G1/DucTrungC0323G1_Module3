36b20f90aa2da9f4a1639ddbbd86a4d126175094
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-06-16 17:14:54 +0700

    first commit

--- s2_bai2.sql
+++ s2_bai2.sql
@@ -14,13 +14,13 @@ CREATE TABLE order_customer (
     id_order INT PRIMARY KEY,
     id_customer INT,
     date_order DATE NOT NULL,
-    total_price_order DOUBLE NOT NULL,
+    total_price_order DOUBLE ,
     FOREIGN KEY (id_customer)
         REFERENCES customer (id_customer)
 );
 CREATE TABLE order_detail (
     PRIMARY KEY (id_order , id_product),
-    od_qty VARCHAR(255),
+    od_qty int,
     id_order INT,
     id_product INT,
     FOREIGN KEY (id_order)
@@ -28,4 +28,43 @@ CREATE TABLE order_detail (
     FOREIGN KEY (id_product)
         REFERENCES product (id_product)
 );
+
+insert into customer
+values
+(1,"Minh Quân",10),
+(2,"Ngoc Oanh", 20),
+(3,"Hong Ha",50);
+
+insert into order_customer
+values
+(1,1,"2006-03-21",null),
+(2,2,"2006-03-23",null),
+(3,1,"2006-03-16",null);
+
+insert into product
+values
+(1,"May Giat",3),
+(2,"Tu Lanh",5),
+(3,"Dieu Hoa",7),
+(4,"Quat",1),
+(5,"Bep Dien",2);
+
+insert into order_detail
+values
+(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+select oc.id_order, oc.date_order, oc.total_price_order
+from order_customer oc;
+
+select customer.name_customer, product.name_product
+from product,customer
+join customer on order_customer.id_customer = customer.id_customer
+join order_customer on order_detail.id_order = order_customer.id_order
+join product on order_detail.id_product = product.id_product
 
