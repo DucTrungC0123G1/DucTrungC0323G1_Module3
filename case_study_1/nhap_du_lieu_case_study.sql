d2a59e3bb889f51763950beaa2363bc809e2b0e5
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-06-18 20:53:33 +0700

    first commit

--- case_study_1/nhap_du_lieu_case_study.sql
+++ case_study_1/nhap_du_lieu_case_study.sql
@@ -111,4 +111,43 @@ values
 (8,	2,	12,	2);
  
  
+select *
+from nhan_vien
+where nhan_vien.ho_ten like "H%" or ho_ten like "T%" or ho_ten like "K%" and length(ho_ten)<=15 ;
+
+select *
+from khach_hang 
+where datediff(now(), ngay_sinh)/365 >=18 and datediff(now(), ngay_sinh)/365 <=50 
+and dia_chi like "%Đà Nẵng%" or dia_chi like "%Quảng Trị%" ;
+
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, count(*) as so_lan_dat
+from khach_hang
+join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+where loai_khach.ten_loai_khach = "Diamond"
+group by hop_dong.ma_khach_hang
+order by so_lan_dat;
+
+select
+    k.ma_khach_hang,
+    k.ho_ten,
+    loai_khach.ten_loai_khach,
+    hop_dong.ma_hop_dong,
+    dich_vu.ten_dich_vu,
+    hop_dong.ngay_lam_hop_dong,
+    hop_dong.ngay_ket_thuc,
+    dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia as tong_tien
+from
+    khach_hang as k
+        left join
+    loai_khach on k.ma_loai_khach = loai_khach.ma_loai_khach
+        left join
+    hop_dong on k.ma_khach_hang = hop_dong.ma_khach_hang
+        left join
+    dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        left join
+    hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        left join
+    dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem;
+ 
 
