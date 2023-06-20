49328e254f87aca1ad24fd729c21ee3825b04552
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-06-21 01:34:55 +0700

    first commit

--- case_study_1/nhap_du_lieu_case_study.sql
+++ case_study_1/nhap_du_lieu_case_study.sql
@@ -259,9 +259,52 @@ group by hop_dong.ma_hop_dong;
 -- Q. 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
 --  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
 
+create view max_dich_vu as
+select dich_vu_di_kem.ma_dich_vu_di_kem, sum(hop_dong_chi_tiet.so_luong) as so_luong
+from dich_vu_di_kem
+join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+group by dich_vu_di_kem.ma_dich_vu_di_kem;
 
+drop view max_dich_vu;
 
+select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem, max_dich_vu.so_luong,max_dich_vu.so_luong
+from max_dich_vu
+join dich_vu_di_kem on max_dich_vu.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+where max_dich_vu.so_luong in (select max(so_luong) from max_dich_vu);
 
+-- Q.14 Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem)
+create view so_lan_su_dung as
+select dich_vu_di_kem.ten_dich_vu_di_kem, count(*) as so_lan_su_dung
+from dich_vu_di_kem
+join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+join hop_dong on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+group by dich_vu_di_kem.ten_dich_vu_di_kem
+having count(*) =1;
+drop view so_lan_su_dung;
+select hop_dong.ma_hop_dong, loai_dich_vu.ten_loai_dich_vu, dich_vu_di_kem.ten_dich_vu_di_kem,so_lan_su_dung.so_lan_su_dung
+from so_lan_su_dung
+join dich_vu_di_kem on so_lan_su_dung.ten_dich_vu_di_kem = dich_vu_di_kem.ten_dich_vu_di_kem
+join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+join hop_dong on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu;
+
+-- Q.15.	Hiển thi thông tin của tất cả nhân viên bao gồm
+ -- ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, 
+ -- dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+ 
+ select nhan_vien.ma_nhan_vien, nhan_vien.ho_ten, trinh_do.ten_trinh_do, bo_phan.ten_bo_phan,nhan_vien.so_dien_thoai,nhan_vien.dia_chi, count(hop_dong.ma_hop_dong) as so_luong_hop_dong
+ from nhan_vien
+ join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+ join trinh_do on nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
+ join bo_phan on nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan
+ where (year(hop_dong.ngay_lam_hop_dong) between 2020 and 2021)
+ group by nhan_vien.ma_nhan_vien
+ having count(hop_dong.ma_hop_dong) <=3;
 
 
 
