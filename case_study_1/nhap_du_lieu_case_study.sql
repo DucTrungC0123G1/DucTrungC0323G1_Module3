360778bc52ad4447d9f4cdf6fbc83a7a79e5a82d
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-06-21 14:17:48 +0700

    first commit

--- case_study_1/nhap_du_lieu_case_study.sql
+++ case_study_1/nhap_du_lieu_case_study.sql
@@ -306,6 +306,64 @@ join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu;
  group by nhan_vien.ma_nhan_vien
  having count(hop_dong.ma_hop_dong) <=3;
 
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+  set sql_safe_updates = 0;
+  delete del
+  from nhan_vien del
+  left join hop_dong on del.ma_nhan_vien = hop_dong.ma_nhan_vien
+  where hop_dong.ma_hop_dong is null;
+  set sql_safe_updates =1;
+  select * from nhan_vien;
+  
+--    Q.17.	Cập nhật thông tin những khách hàng có 
+-- ten_loai_khach từ Platinum lên Diamond, 
+-- chỉ cập nhật những khách hàng đã từng đặt phòng với
+-- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+
+SET foreign_key_checks = 0;
+update khach_hang
+join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+set khach_hang.ma_loai_khach = 1
+where khach_hang.ma_loai_khach =2 and (dich_vu.chi_phi_thue + (hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) > 10000000 )and (year(hop_dong.ngay_lam_hop_dong) =2021);
+
+SET foreign_key_checks = 1;
+select * from khach_hang
+join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach;
+
+-- Q.18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).-- 
+
+SET foreign_key_checks = 0;
+delete hd
+from hop_dong hd
+join khach_hang on hd.ma_khach_hang = khach_hang.ma_khach_hang
+where year(hd.ngay_lam_hop_dong) < 2021;
+set foreign_key_checks =1;
+select * 
+from khach_hang
+join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang;
+
+-- Q.19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+
+update dich_vu_di_kem
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+set dich_vu_di_kem.gia = dich_vu_di_kem.gia*2
+where hop_dong_chi_tiet.so_luong>10;
+
+select dich_vu_di_kem.ma_dich_vu_di_kem,dich_vu_di_kem.gia,dich_vu_di_kem.ten_dich_vu_di_kem
+from dich_vu_di_kem;
+
+-- Q.20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, 
+-- thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi. 
+
+select nhan_vien.ma_nhan_vien as "ID",nhan_vien.ho_ten,nhan_vien.email,nhan_vien.so_dien_thoai,nhan_vien.ngay_sinh,nhan_vien.dia_chi, "Nhan Vien" as 'Kiểu'
+from nhan_vien
+union all
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, khach_hang.email,khach_hang.so_dien_thoai,khach_hang.ngay_sinh,khach_hang.dia_chi , "Khach Hang"
+from khach_hang
 
 
 
