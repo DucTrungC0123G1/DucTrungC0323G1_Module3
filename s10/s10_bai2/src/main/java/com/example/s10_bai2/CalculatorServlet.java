3370e4b0cc009b676c1fff531974e1799bdac36f
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-07-01 11:54:26 +0700

    NopBai

--- s10/s10_bai2/src/main/java/com/example/s10_bai2/CalculatorServlet.java
+++ s10/s10_bai2/src/main/java/com/example/s10_bai2/CalculatorServlet.java
@@ -9,24 +9,30 @@ import java.io.IOException;
 public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    double num1 = Double.parseDouble(request.getParameter("num1"));
-    double num2 = Double.parseDouble(request.getParameter("num2"));
-    double result = 0;
-    if (request.getParameter("select").equals("cong")){
-        result = num1+num2;
-    }
-    if (request.getParameter("select").equals("tru")){
-            result = num1-num2;
+        double num1 = Double.parseDouble(request.getParameter("num1"));
+        double num2 = Double.parseDouble(request.getParameter("num2"));
+        String result = null;
+        if (request.getParameter("select").equals("cong")) {
+            result = String.valueOf(num1 + num2);
         }
-    if (request.getParameter("select").equals("nhan")){
-            result = num1*num2;
+        if (request.getParameter("select").equals("tru")) {
+            result = String.valueOf(num1 - num2);
         }
-    if (request.getParameter("select").equals("chia")){
-            result = num1/num2;
+        if (request.getParameter("select").equals("nhan")) {
+            result = String.valueOf(num1 * num2);
         }
-    request.setAttribute("result",result);
-    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/calculator.jsp");
-    requestDispatcher.forward(request,response);
+//    if (request.getParameter("select").equals("chia")){
+//            result = num1/num2;
+//        }
+        if (request.getParameter("select").equals("chia") && (num2 == 0)) {
+            result = " Second Number Phải Khác 0";
+        } else if (request.getParameter("select").equals("chia")) {
+            result = String.valueOf(num1 / num2);
+        }
+
+        request.setAttribute("result", result);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/calculator.jsp");
+        requestDispatcher.forward(request, response);
     }
 
     @Override
